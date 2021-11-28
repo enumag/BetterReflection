fcf0fd406cd289ac4c1c191f5e6a40998f7e19c7
Ondrej Mirtes <ondrej@mirtes.cz>
2021-11-28 14:58:07 +0100

    Attributes - do not instantiate the adapter between PHP 8.0.0 and 8.0.11

--- src/Reflection/Adapter/ReflectionClassConstant.php
+++ src/Reflection/Adapter/ReflectionClassConstant.php
@@ -15,6 +15,8 @@ use function array_map;
 use function constant;
 use function sprintf;
 
+use const PHP_VERSION_ID;
+
 final class ReflectionClassConstant extends CoreReflectionClassConstant
 {
     public const IS_FINAL = 32;
@@ -133,6 +135,10 @@ final class ReflectionClassConstant extends CoreReflectionClassConstant
     {
         if ($flags !== 0 && $flags !== ReflectionAttribute::IS_INSTANCEOF) {
             throw new ValueError('Argument #2 ($flags) must be a valid attribute filter flag');
+        }
+
+        if (PHP_VERSION_ID >= 80000 && PHP_VERSION_ID < 80012) {
+            return [];
         }
 
         if ($name !== null && $flags & ReflectionAttribute::IS_INSTANCEOF) {

0fdb6e5a07d33ba1a1d110cfe9e3d58c4fdc61c1
Ondrej Mirtes <ondrej@mirtes.cz>
2021-12-16 11:59:14 +0100

    FakeReflectionAttribute for PHP < 7.2 and 8.0.0..8.0.12

--- src/Reflection/Adapter/ReflectionParameter.php
+++ src/Reflection/Adapter/ReflectionParameter.php
@@ -17,8 +17,6 @@ use ValueError;
 use function array_map;
 use function sprintf;
 
-use const PHP_VERSION_ID;
-
 /**
  * @psalm-suppress MissingImmutableAnnotation
  */
@@ -154,7 +152,7 @@ final class ReflectionParameter extends CoreReflectionParameter
     /**
      * @param class-string|null $name
      *
-     * @return list<ReflectionAttribute>
+     * @return list<ReflectionAttribute|FakeReflectionAttribute>
      */
     public function getAttributes(?string $name = null, int $flags = 0): array
     {
@@ -162,15 +160,7 @@ final class ReflectionParameter extends CoreReflectionParameter
             throw new ValueError('Argument #2 ($flags) must be a valid attribute filter flag');
         }
 
-        if (PHP_VERSION_ID >= 80000 && PHP_VERSION_ID < 80012) {
-            return [];
-        }
-
-        if (PHP_VERSION_ID < 70200) {
-            return [];
-        }
-
-        if ($name !== null && $flags & ReflectionAttribute::IS_INSTANCEOF) {
+        if ($name !== null && $flags !== 0) {
             $attributes = $this->betterReflectionParameter->getAttributesByInstance($name);
         } elseif ($name !== null) {
             $attributes = $this->betterReflectionParameter->getAttributesByName($name);
@@ -178,7 +168,7 @@ final class ReflectionParameter extends CoreReflectionParameter
             $attributes = $this->betterReflectionParameter->getAttributes();
         }
 
-        return array_map(static fn (BetterReflectionAttribute $betterReflectionAttribute): ReflectionAttribute => new ReflectionAttribute($betterReflectionAttribute), $attributes);
+        return array_map(static fn (BetterReflectionAttribute $betterReflectionAttribute): ReflectionAttribute|FakeReflectionAttribute => ReflectionAttributeFactory::create($betterReflectionAttribute), $attributes);
     }
 
     public function __get(string $name): mixed
